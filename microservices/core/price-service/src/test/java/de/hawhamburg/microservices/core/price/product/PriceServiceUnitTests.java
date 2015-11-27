a78dc763fb3e9ecc789f42a2bfc455cd79eb8311
Michi Henero <henero@gmx.de>
2015-11-27 15:51:13 +0100

    Unittest for Price-Core with mocking

--- microservices/core/price-service/src/test/java/de/hawhamburg/microservices/core/price/product/PriceServiceUnitTests.java
+++ microservices/core/price-service/src/test/java/de/hawhamburg/microservices/core/price/product/PriceServiceUnitTests.java
@@ -1,12 +1,15 @@
 package de.hawhamburg.microservices.core.price.product;
 
 import de.hawhamburg.microservices.core.price.jpa.domain.Price;
-import de.hawhamburg.microservices.core.price.jpa.repository.PriceRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.IntegrationTest;
+import de.hawhamburg.microservices.core.price.jpa.service.PriceService;
+import de.hawhamburg.microservices.core.price.jpa.service.PriceServiceImpl;
+import org.junit.Before;
+import org.mockito.Mockito;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 //import org.junit.BeforeClass;
@@ -15,63 +18,44 @@ import java.util.UUID;
 /**
  * Created by Michael B. on 18.11.2015.
  */
-@IntegrationTest
 public class PriceServiceUnitTests {
 
-    //private PriceService priceService;
-    @Autowired
-    private PriceRepository priceRepository;
+    private PriceService priceService;
 
-    //@Test
-    //public void test
-
-    //@BeforeClass
-    //public void Setuo() {
-        //PriceService priceService = new PriceServiceImpl();
-    //}
-
-
-    @Test
-    public void TestPriceForFlight(){
-
-        //UUID anlegen
-        //UUID id = UUID.randomUUID();
-        UUID id = UUID.fromString("0b4acc1d-3439-4b67-905a-1f7a4bb692cd");
-        //Preisobjekt mit Preis:100 und einer UUID anlegen
-        Price price = new Price.PriceBuilder().withFlightId(id).withValue(100).build();
-        //Preis in der Datenbank abspeichern
-
-        priceRepository.save(price);
-        //priceService.createPrice(price);
-
-        //PriceServiceImpl priceServiceImpl = new PriceServiceImpl() {};
-
-        Assert.assertEquals(price,priceRepository.findByFlightId(id));
-
-
-        //Preis erstellen
-        //priceServiceImpl.createPrice(price);
-
-
-
-        //priceService.createPrice(price);
-        //priceRepository.save(price);
-
-        System.out.println(price.getValue());
-        //Assert.assertEquals(110.0,priceService.priceForFlight(id2));
-        //Assert.assertEquals(100, 100);
+    @Before
+    public void init(){
+        this.priceService = new PriceServiceImpl();
     }
 
+    @Test
+    public void TestPriceForFlight() {
 
-//    Price priceForFlight(UUID flightId);
-//
-//    List<Price> findAllPrices();
-//
-//    Price createPrice(Price price);
-//
-//    void removePrice(UUID flightId);
+        //UUID anlegen
+        UUID id = UUID.fromString("0b4acc1d-3439-4b67-905a-1f7a4bb692ca");
+        UUID id2 = UUID.fromString("1b4acc1d-3439-4b67-905a-1f7a4bb692cb");
+        UUID id3 = UUID.fromString("2b4acc1d-3439-4b67-905a-1f7a4bb692cc");
+        //Preisobjekt mit Preis:100 und einer UUID anlegen
+        Price price = new Price.PriceBuilder().withFlightId(id).withValue(100).build();
+        Price price2 = new Price.PriceBuilder().withFlightId(id2).withValue(100).build();
+        Price price3 = new Price.PriceBuilder().withFlightId(id3).withValue(200).build();
 
-//    void updatePrice(Price price);
+        List<Price> allPrices = new ArrayList<>();
+        allPrices.add(price);
+        allPrices.add(price2);
+        allPrices.add(price3);
 
 
+        //Objekte & Methoden mocken
+        priceService = Mockito.mock(PriceServiceImpl.class);
+        Mockito.when(priceService.createPrice(price)).thenReturn(price2);
+        Mockito.when(priceService.createPrice(price2)).thenReturn(price);
+        Mockito.when(priceService.createPrice(price3)).thenReturn(price3);
+        Mockito.when(priceService.priceForFlight(id)).thenReturn(price);
+        Mockito.when(priceService.findAllPrices()).thenReturn(allPrices);
+
+        //Tests
+        Assert.assertEquals(price2.getValue(), priceService.createPrice(price).getValue());
+        Assert.assertEquals(100.0, priceService.priceForFlight(id).getValue());
+        Assert.assertEquals(allPrices ,priceService.findAllPrices());
+    }
 }

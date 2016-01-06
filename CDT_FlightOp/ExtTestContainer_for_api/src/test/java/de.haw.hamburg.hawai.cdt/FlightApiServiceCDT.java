2bbc41ee47b4cf24e690014bedfc6f80ff9723b0
Michi Henero <henero@gmx.de>
2016-01-06 15:34:05 +0100

    bugfixes CDT-Test for flightOp and reservation

---
+++ CDT_FlightOp/ExtTestContainer_for_api/src/test/java/de.haw.hamburg.hawai.cdt/FlightApiServiceCDT.java
@@ -0,0 +1,212 @@
+package de.haw.hamburg.hawai.cdt;
+
+import com.google.gson.*;
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.parsing.Parser;
+import com.jayway.restassured.response.ResponseBody;
+import com.jayway.restassured.specification.RequestSpecification;
+import com.sun.org.apache.xpath.internal.operations.NotEquals;
+import org.mockito.Mockito;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+
+/**
+ * Created by unknown on 02.12.15.
+ */
+public class FlightApiServiceCDT {
+    private String token;
+
+    /**
+     * The URL under which the Service is accessable.
+     * On pure-local deployments this is usually 192.168.99.100
+     * when Running Docker on OS X or Windows.
+     * Change to 127.0.0.1 when using Linux.
+     * Change to the real IP Address if using a production like environment.
+     */
+//    private String apiServiceBaseURL = "127.0.0.1";
+    private String baseAddress = "";
+
+    @BeforeClass
+    public void setupSecurityToken() {
+        setupSystemIpConfig();
+        // Define relaxed SSL Auth, since we use self-signed certificates
+        RestAssured.useRelaxedHTTPSValidation();
+        // Register JSON Parser for plain text responses
+        RestAssured.registerParser("text/plain", Parser.JSON);
+        // Order and extract auth-token
+        token =
+                given().
+                        param("grant_type", "password").
+                        param("client_id", "acme").
+                        param("username", "user").
+                        param("password", "password").
+                        when().post("https://acme:acmesecret@" + baseAddress + ":9999/uaa/oauth/token").then().
+                        extract().path("access_token");
+    }
+
+    private void setupSystemIpConfig() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.indexOf("win") >= 0 || os.indexOf("mac") >= 0) {
+            baseAddress = "192.168.99.100";
+        } else if (os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0 || os.indexOf("aix") > 0) {
+            baseAddress = "127.0.0.1";
+        }
+    }
+
+//    @Test
+//    public void TestFlightsRestGet() {
+//        given().
+////                // use auth token for oauth v2 header
+//        auth().oauth2(token).
+////                // make call to API
+////                        when().get("https://" + apiServiceBaseURL + "/api/api/flight")
+////                .then()
+////                .body("size()", greaterThanOrEqualTo(0));
+//        String url = "http://localhost:8080/prices2";
+//        given().get(url).then().body("size()", greaterThanOrEqualTo(0));
+//        given().get(url).then().body("get(0).flightId", equalTo("76fc2e8d-7587-4a18-b2f8-33eeda154b8b"));
+//        String json = given().get(url).getBody().print();
+//        System.out.println(json);
+//
+//        JsonParser parser = new JsonParser();
+//        JsonArray arr = parser.parse(json).getAsJsonArray();
+//        for(JsonElement jsonElement : arr){
+//            JsonObject jsonObject = jsonElement.getAsJsonObject();
+//            System.out.println(jsonObject.get("flightId").getAsString());
+//        }
+//    }
+
+    @Test
+    public void TestGetWantedFlights() {
+//        given().
+//                // use auth token for oauth v2 header
+//                        auth().oauth2(token).
+//                // make call to API
+//                        when().get("https://" + baseAddress + "/api/api/flight")
+//                .then()
+//                .body("size()", greaterThanOrEqualTo(0));
+
+
+//        {flight:{delay:"bla", startTime:"11:00", }
+
+        String url = "http://localhost:8080/api/api/flight";
+
+        String json = given().auth().oauth2(token).get(url).getBody().print();
+
+//      TODO - ggf. müssen die Zugriffe anhand des JsonObjektes angepasst werden z.B. flight.get(0)....
+
+//      Prüft die Länge des Arrays mit Flügen >= 1
+        given().auth().oauth2(token).get(url).then().body("size()", greaterThan(0));
+//      Prüfen ob Delay !=null ist
+        given().auth().oauth2(token).get(url).then().body("get(0).delay", notNullValue());
+//      Prüft die Länge der FlightId(36)
+        given().auth().oauth2(token).get(url).then().body("get(0).blueprint.flightnumber.length()", equalTo(36));
+//      Prüft ob Flightnumber eine UUID ist
+        given().auth().oauth2(token).get(url).then().body("get(0).blueprint.flightnumber", instanceOf(UUID.class));
+//      Prüfen ob Startzeit !=null ist
+        given().auth().oauth2(token).get(url).then().body("get(0).startTime", notNullValue());
+//      Prüft die Länge der AircraftId(36)
+        given().auth().oauth2(token).get(url).then().body("get(0).aircraft.length()", equalTo(36));
+//      Prüft ob AircraftId eine UUID ist
+        given().auth().oauth2(token).get(url).then().body("get(0).aircraft", instanceOf(UUID.class));
+//      Prüfen ob Departure !=null ist
+        given().auth().oauth2(token).get(url).then().body("get(0).blueprint.departure", notNullValue());
+//      Prüfen ob Destination !=null ist
+        given().auth().oauth2(token).get(url).then().body("get(0).blueprint.destination", notNullValue());
+//      Prüfen ob Duration !=null ist
+        given().auth().oauth2(token).get(url).then().body("get(0).blueprint.duration", notNullValue());
+//      Prüfen ob Aircraftmodel !=null ist
+        given().auth().oauth2(token).get(url).then().body("get(0).blueprint.aircraftmodel", notNullValue());
+
+    }
+
+//    @Test
+//    public void TestFlightsRestGet() {
+////        given().
+////                // use auth token for oauth v2 header
+////                        auth().oauth2(token).
+////                // make call to API
+////                        when().get("https://" + apiServiceBaseURL + "/api/api/flight")
+////                .then()
+////                .body("size()", greaterThanOrEqualTo(0));
+//        String url = "http://localhost:8080/prices";
+//        given().get(url).then().body("size()", greaterThanOrEqualTo(0));
+//        given().get(url).then().body("list.get(0).flightId", equalTo("76fc2e8d-7587-4a18-b2f8-33eeda154b8b"));
+//        String json = given().get(url).getBody().print();
+//        System.out.println(json);
+//
+//        JsonParser parser = new JsonParser();
+//        JsonArray arr = parser.parse(json).getAsJsonArray();
+//        for(JsonElement jsonElement : arr){
+//            JsonObject jsonObject = jsonElement.getAsJsonObject();
+//            System.out.println(jsonObject.get("flightId").getAsString());
+//        }
+//    }
+
+
+
+
+
+
+//    @Test
+//    public void TestGetWantedFlights() {
+////        given().
+////                // use auth token for oauth v2 header
+////                        auth().oauth2(token).
+////                // make call to API
+////                        when().get("https://" + apiServiceBaseURL + "/api/api/flight")
+////                .then()
+////                .body("size()", greaterThanOrEqualTo(0));
+//
+//
+////        {flight:{delay:"bla", startTime:"11:00", }
+//
+//        String url = "http://localhost:8080/api/api/flight";
+//
+//        String json = given().get(url).getBody().print();
+//
+////      TODO - ggf. müssen die Zugriffe anhand des JsonObjektes angepasst werden z.B. flight.get(0)....
+//
+////      Prüft die Länge des Arrays mit Flügen >= 1
+//        given().get(url).then().body("size()", greaterThan(0));
+////      Prüft die Stringlänge des Delay
+//        given().get(url).then().body("get(0).delay.length()", greaterThanOrEqualTo(0));
+////      Prüfen ob Delay !=null ist
+//        given().get(url).then().body("get(0).delay.length()", greaterThanOrEqualTo(0));
+////      Prüft die Länge der FlightId(36)
+//        given().get(url).then().body("get(0).blueprint.flightnumber.length()", equalTo(36));
+////      Prüft die Stringlänge der Startzeit
+//        given().get(url).then().body("get(0).startTime.length()", greaterThan(0));
+////      Prüft die Länge der AircraftId(36)
+//        given().get(url).then().body("get(0).aircraft.length()", equalTo(36));
+////      Prüft die Stringlänge der Departure
+//        given().get(url).then().body("get(0).blueprint.departure.length()", greaterThanOrEqualTo(0));
+////      Prüft die Stringlänge der Destination
+//        given().get(url).then().body("get(0).blueprint.destination.length()", greaterThanOrEqualTo(0));
+////      Prüft die Stringlänge der Duration
+//        given().get(url).then().body("get(0).blueprint.duration.length()", greaterThanOrEqualTo(0));
+////      Prüft die Stringlänge des Aircraftmodel
+//        given().get(url).then().body("get(0).blueprint.aircraftmodel.length()", greaterThanOrEqualTo(0));
+//
+//
+//
+////      Prüft die Stringlänge des Aircraftmodel
+//        given().get(url).then().body("get(0).blueprint.aircraftmodel.length()", isEmptyOrNullString());
+//        given().get(url).then().body("get(0).blueprint.aircraftmodel.length()", IsEmptyString.isEmptyOrNullString());
+//        given().get(url).then().body("get(0).blueprint.aircraftmodel.length()", notNullValue());
+//
+//    }
+
+//      /api/api/flight
+//      getWantedFlights - /flight
+//      askForReservations
+
+}
---
+++ CDT_Reservation/ExtTestContainer_for_api/src/test/java/de.haw.hamburg.hawai.cdt/ReservationApiServiceCDT.java
@@ -0,0 +1,121 @@
+package de.haw.hamburg.hawai.cdt;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.parsing.Parser;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static com.jayway.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
+/**
+ * This class is an example of CDTs.
+ */
+@Test
+public class ProductApiServiceCDT {
+
+    private String token;
+
+    /**
+     * The URL under which the Service is accessable.
+     * On pure-local deployments this is usually 192.168.99.100
+     * when Running Docker on OS X or Windows.
+     * Change to 127.0.0.1 when using Linux.
+     * Change to the real IP Address if using a production like environment.
+     */
+//    private String apiServiceBaseURL = "192.168.99.100";
+    private String baseAddress = "";
+
+    @BeforeClass
+    public void setupSecurityToken(){
+        setupSystemIpConfig();
+        // Define relaxed SSL Auth, since we use self-signed certificates
+        RestAssured.useRelaxedHTTPSValidation();
+        // Register JSON Parser for plain text responses
+        RestAssured.registerParser("text/plain", Parser.JSON);
+        // Order and extract auth-token
+        token =
+                given().
+                        param("grant_type", "password").
+                        param("client_id", "acme").
+                        param("username", "user").
+                        param("password", "password").
+                        when().post("https://acme:acmesecret@" + apiServiceBaseURL + ":9999/uaa/oauth/token").then().
+                        extract().path("access_token");
+
+    }
+
+    private void setupSystemIpConfig() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.indexOf("win") >= 0 || os.indexOf("mac") >= 0) {
+            baseAddress = "192.168.99.100";
+        } else if (os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0 || os.indexOf("aix") > 0) {
+            baseAddress = "127.0.0.1";
+        }
+    }
+
+    @Test
+    public void TestGetReservationForFlights(){
+//        given().
+//                // use auth token for oauth v2 header
+//                        auth().oauth2(token).
+//                // make call to API
+//                        when().get("https://" + baseAddress + "/api/tickets/flight/")
+//                        when().get("https://" + baseAddress + "/api/flights")
+//                .then()
+//                .body("size()", greaterThanOrEqualTo(0));
+
+
+//      TODO - fraglich ob das so läuft
+        String urlId = "http://localhost:8080/api/flights";
+        String json = given().auth().oauth2(token).get(urlId).getBody().print();
+
+        JsonParser parser = new JsonParser();
+        JsonArray arr = parser.parse(json).getAsJsonArray();
+        JsonElement jsonElement = arr.get(0);
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+        UUID testId = UUID.fromString(jsonObject.get("flightId").getAsString());
+
+
+//      TODO - Für welche FlightId sollen die Reservierungen angefragt werden??
+//        UUID id = UUID.fromString("76fc2e8d-7587-4a18-b2f8-33eeda154b8b");
+
+        String url = "http://localhost:8080/api/tickets/flight/";
+
+//      TODO - hier muss die Id ggf. gewechselt werden
+        String json = given().auth().oauth2(token).get(url+testId).getBody().print();
+
+//      Prüft die Länge des Arrays mit Flügen >= 1
+        given().auth().oauth2(token).get(url+testId).then().body("size()", greaterThan(0));
+
+//      Prüfen ob TicketId !=null ist
+        given().auth().oauth2(token).get(url+testId).then().body("get(0).ticketId", notNullValue());
+
+//      Prüfen ob TicketId ist instanceOf Integer
+        given().auth().oauth2(token).get(url+testId).then().body("get(0).ticketId", instanceOf(Integer.class));
+
+//      Prüfen ob passengerArray !=null ist
+        given().auth().oauth2(token).get(url+testId).then().body("get(0).passenger", notNullValue());
+
+//      Prüfen ob reservationArray !=null ist
+        given().auth().oauth2(token).get(url+testId).then().body("get(0).reservation", notNullValue());
+
+//      Prüfen ob in Reservation eine Buchungsart vorhanden ist
+        given().auth().oauth2(token).get(url+testId).then().body("get(0).reservation.reservationType", notNullValue());
+
+//      Prüft ob ReservationType vom Typ "Eco", "Busi" oder "First" ist
+//        given().get(url).then().body("get(0).reservation.reservationType", contains("Economy") || contains("Business"));
+
+//      Prüfen ob in Reservation die ReservationId ist instanceOf Integer
+        given().auth().oauth2(token).get(url+testId).then().body("get(0).reservation.reservationId", instanceOf(Integer.class));
+
+//      Prüfen ob Bookingtype !=null ist
+        given().auth().oauth2(token).get(url+testId).then().body("get(0).bookingType", notNullValue());
+//      Prüfen ob TicketDate !=null ist
+        given().auth().oauth2(token).get(url+testId).then().body("get(0).ticketDate.length()", greaterThan(0));
+
+
+//      Prüfen ob in Reservation CheckedIn ist instanceOf Boolean
+//        given().get(url).then().body("get(0).reservation.checkedIn", instanceOf(Boolean.class));
+    }
+}

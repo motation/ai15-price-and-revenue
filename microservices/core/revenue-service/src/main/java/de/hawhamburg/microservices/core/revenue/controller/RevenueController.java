8aa5d01a312aef653366c6401c7f8fa3b9e8bc19
Sven-Ole Fedders <motation@github.com>
2016-01-15 12:01:40 +0100

    rev controller update

--- microservices/core/revenue-service/src/main/java/de/hawhamburg/microservices/core/revenue/controller/RevenueController.java
+++ microservices/core/revenue-service/src/main/java/de/hawhamburg/microservices/core/revenue/controller/RevenueController.java
@@ -6,6 +6,7 @@ import de.hawhamburg.microservices.core.revenue.util.WrappedList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +14,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 
 import java.io.*;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -33,12 +35,12 @@ public class RevenueController {
     private RevenueService revenueService;
 
     @RequestMapping(value = "/revenue/{flightId}", method = RequestMethod.GET)
-    public Revenue getRevenue(@PathVariable final UUID flightId){
+    public Revenue getRevenue(@PathVariable final UUID flightId) {
         return revenueService.revenueForFlight(flightId);
     }
 
     @RequestMapping(value = "/revenues", method = RequestMethod.GET)
-    public WrappedList findAllRevenues(){
+    public WrappedList findAllRevenues() {
         return WrappedList.getWrappedList(Revenue.class, revenueService.findAllRevenues());
     }
 
@@ -46,14 +48,15 @@ public class RevenueController {
     public Revenue createRevenue(@RequestBody final Revenue revenue, HttpServletResponse response) throws IOException {
         Revenue savedRevenueItem = revenueService.createRevenue(revenue);
         // OF maybe build Error Entity!
-        if(savedRevenueItem != null){
+        if (savedRevenueItem != null) {
             response.setStatus(HttpServletResponse.SC_CREATED);
         } else {
             response.setStatus(HttpServletResponse.SC_CONFLICT);
         }
         return savedRevenueItem;
     }
-    @RequestMapping(value="/revenues/{flightId}", method = RequestMethod.DELETE)
+
+    @RequestMapping(value = "/revenues/{flightId}", method = RequestMethod.DELETE)
     public void deleteRevenue(@PathVariable final UUID flightId, HttpServletResponse response) throws IOException {
         try {
             revenueService.removeRevenue(flightId);
@@ -63,7 +66,7 @@ public class RevenueController {
         }
     }
 
-    @RequestMapping(value="/revenue/{flightId}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/revenue/{flightId}", method = RequestMethod.PUT)
     public void deleteRevenue(@RequestBody final Revenue revenue, HttpServletResponse response) throws IOException {
         try {
             revenueService.updateRevenue(revenue);
@@ -73,11 +76,21 @@ public class RevenueController {
         }
     }
 
-    @RequestMapping(value="/revenues/{startTime}/{endTime}", method = RequestMethod.GET)
-    public List<Revenue> getRevenuesForTime(@PathVariable long startTime,@PathVariable long endTime){
-        LOG.info("startTime="+startTime+" endTime="+endTime);
-        List<Revenue> revenues = revenueService.revenuesForTime(startTime,endTime);
+    @RequestMapping(value = "/revenues/{startTime}/{endTime}", method = RequestMethod.GET)
+    public List<Revenue> getRevenuesForTime(@PathVariable long startTime, @PathVariable long endTime) {
+        LOG.info("startTime=" + startTime + " endTime=" + endTime);
+        List<Revenue> revenues = revenueService.revenuesForTime(startTime, endTime);
         return revenues;
+    }
+
+    @RequestMapping(value = "/rev", method = RequestMethod.GET)
+    public List<Revenue> getRevenueFor(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.TIME,
+            pattern = "yyyy-MM-dd HH:mm:ss") final Date fromDate,
+                                 @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.TIME,
+                                         pattern = "yyyy-MM-dd HH:mm:ss") final Date toDate) {
+        System.out.println(fromDate.toString());
+        System.out.println(toDate.toString());
+        return revenueService.revenuesForTime(fromDate.getTime(),toDate.getTime());
     }
 
 }
